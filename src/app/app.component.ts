004d66be5409ac1a8da69468e9ecef61c767841b
王广开 <416266475@qq.com>
2018-01-08 18:09:07 +0800

    。。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export class LoginForm {
+  strUserName: string;
+  strUserPassword: string;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './login/login.html',
+  styleUrls: ['./login/login.css'],
+  /*template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`*/
 })
 export class AppComponent {
   title = 'My First Angular App';
+  hero = 'Windstorm';
 }
+
+
+loginForm:LoginForm = {
+  strUserName: 'zhang',
+  strUserPassword: ''
+};
